7283df27ead00ce8fd233e4f3bacb1c21b5b5232
Roman Kolivashko <rkolivashko@gmail.com>
2021-12-07 11:21:12 -0800

    Added NavLinks

--- src/App.js
+++ src/App.js
@@ -1,22 +1,28 @@
 import "./App.css";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes, NavLink } from "react-router-dom";
+ 
+// Pages
 import Home from "./pages/Home";
 import Contact from "./pages/Contact";
 import About from "./pages/About";
-
+ 
 function App() {
   return (
     <div className="App">
-      <nav>
-        <h1>My Articles</h1>
-      </nav>
-      <Router>
+      <BrowserRouter>
+        <nav>
+          <h1>My Articles</h1>
+          <NavLink to="/">Home</NavLink>
+          <NavLink to="about">About</NavLink>
+          <NavLink to="contact">Contact</NavLink>
+        </nav>
+ 
         <Routes>
-          <Route exact path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
+          <Route path="/" element={<Home />}></Route>
+          <Route path="contact" element={<Contact />}></Route>
+          <Route path="about" element={<About />}></Route>
         </Routes>
-      </Router>
+      </BrowserRouter>
     </div>
   );
 }
