74980de95baa3bf555b45d8aced8e830dca94e54
Roman Kolivashko <rkolivashko@gmail.com>
2021-12-07 12:40:28 -0800

    Added useParams hook to read artcile data by id

--- src/pages/Article.js
+++ src/pages/Article.js
@@ -1,7 +1,25 @@
+import { useParams } from "react-router-dom";
+import { useFetch } from "../hooks/useFetch";
+
 export default function Article() {
+  //const params = useParams()
+  //another way
+  const { id } = useParams();
+  const url = "http://localhost:8000/articles/" + id;
+  const { data: article , isPending, error } = useFetch(url)
+
+  //return <div>Article page - { params.id }</div>;
+  //return <div>Article page - {id}</div>;
   return (
     <div>
-      Article page
+      {isPending && <div>Loading ...</div>}
+      {error && <div>{error}</div>}
+      {article && (<div>
+        <h2>{article.title}</h2>
+        <p>By {article.author}</p>
+        <p>{article.body}</p>
+        </div>)}
     </div>
   )
+
 }
